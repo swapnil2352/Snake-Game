bd4c26a1f135795c0f86e82e581b15517285f9e4
Swapnil Saxena <swapnilsaxena235@gmail.com>
2024-09-05 00:52:45 +0530

    Added Move Elements in the game

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 // HTML Elements
 const board = document.getElementById("game-board")
+const instructionText = document.getElementById("instruction-text")
+const logo = document.getElementById("logo")
 
 //Game variables
 const gridSize = 20
 let snake = [{x:10,y:10}]  // start from the mid of 20x20 board. 
 let food  = generateFood()
- 
+let direction = 'right'
+let gameInterval
+let gameSpeedDelay = 200
+let gameStarted = false 
 
 // Draw game map, snake and food. 
 function draw(){
@@ -38,7 +43,7 @@ function setPosition(element, position){
 }
 
 // Testing draw fn 
-draw();
+// draw();
 
 // Draw food function 
 function drawFood(){
@@ -54,8 +59,100 @@ function generateFood(){
     return {x,y}
 }
 
-// Moving the snake
+// Moving the snake   *** We are using shallow copy here, as we don't want to do any changes in the org arr. It can create some errors. 
 function move(){
-
+    const head = {...snake[0]}
+    switch (direction) {
+        case 'right':
+            head.x++;
+            break;
+        case 'up':
+            head.y--;
+            break;
+        case 'left':
+            head.x--;
+            break;
+        case 'down':
+            head.y++;
+            break;    
+    }
+    // unshift will add a head object prior to all the objects in the snake array. 
+    snake.unshift(head)  
+    // snake.pop()
+    if(head.x === food.x && head.y === food.y){
+        food = generateFood();
+        increaseSpeed()
+        clearInterval(gameInterval); // clear past Interval
+        gameInterval = setInterval(()=>{
+            move()
+            // checkCollision()
+            draw()
+        }, gameSpeedDelay)
+    }
+    else{
+        snake.pop()
+    }
 }
 
+//test moving
+// setInterval(()=>{
+//     move() // move first 
+//     draw() // then draw again the new position 
+// },200)
+
+
+// Start game function 
+function startGame(){
+    gameStarted = true // keep track of a running game 
+    instructionText.style.display = 'none'
+    logo.style.display = 'none'
+    gameInterval = setInterval(()=>{
+        move()
+        // checkCollision()
+        draw()
+    },gameSpeedDelay)
+}
+
+// Keypress event listener 
+function handleKeyPress(event){   // if condition to handle every browser. 
+    if((!gameStarted && event.code == 'Space') || (!gameStarted && event.key == ' ') ){
+        startGame()
+    }
+    else{
+        switch(event.key){
+            case 'ArrowUp':
+                direction = 'up'
+                break
+            case 'ArrowDown':
+                direction = 'down'
+                break
+            case 'ArrowLeft':
+                direction = 'left'
+                break
+            case 'ArrowRight':
+                direction = 'right'
+                break
+        }
+    }
+}
+
+document.addEventListener('keydown', handleKeyPress) // for every keydown we are calling the function handleKeyPress. 
+
+function increaseSpeed(){
+    if(gameSpeedDelay > 150){
+        gameSpeedDelay -= 5
+    }
+    else if (gameSpeedDelay > 100){
+        gameSpeedDelay -= 3;
+    }
+    else if (gameSpeedDelay > 50){
+        gameSpeedDelay -= 2;
+    }
+    else if (gameSpeedDelay > 25){
+        gameSpeedDelay -= 1;
+    }
+}
+
+function checkCollision(){
+    
+}
